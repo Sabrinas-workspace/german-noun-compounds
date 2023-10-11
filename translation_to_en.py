429aa19d459211219c142bc86dd5291b4b1e20c7
Sabrina Kenter <88004840+Sabrinas-workspace@users.noreply.github.com>
2023-10-11 15:30:05 +0200

    Update translation_to_en.py

--- translation_to_en.py
+++ translation_to_en.py
@@ -4,10 +4,13 @@
 import deepl
 import pandas as pd
 
-auth_key = "key"  # Replace "key" with your key
+auth_key = "key"  # Replace "key" with your authentication key, more info: https://support.deepl.com/hc/en-us/articles/360020695820-Authentication-Key
 translator = deepl.Translator(auth_key)
 
 df = pd.read_csv('dataset_with_meanings.txt', sep="\t", header=0)
+
+# For the analysis data set from meaning_random_50_analysis.py use the following line instead
+# df = pd.read_csv('random_50_duden_result_preprocessed.txt', sep='\t', header=0)
 
 en_translation = []
 
@@ -18,3 +21,15 @@ for meaning in df['Meaning']:
 df['Translation'] = en_translation
 
 df.to_csv('dataset_with_translations.txt', sep='\t', index=False)
+
+# For the analysis data set from meaning_random_50_analysis.py use the following line instead
+# df.to_csv('random_50_dataset_with_translations.txt', sep='\t', index=False)
+
+
+# Creates the data set which is used for the classification with transfomers (not needed for data set analysis)
+df_transformers = df[['Translation', 'Relation']]
+
+# Deletes all rows with LEX compounds
+df_transformers  = df_transformers.loc[df["Relation"] != "LEX"]
+
+df_transformers.to_csv('only_translation_relation.txt', sep=';', header=None, index=False)
